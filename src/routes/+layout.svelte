9fa540183b88f7b912e3a3e4980f2fc1445672cd
Stephen Loiola Bastos <stephen.loiola@actency.fr>
2026-03-09 19:14:20 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import "../app.css";
+</script>
+
+<main class="bg-background min-h-dvh flex justify-center py-8 ">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+
+
+<section class="w-full max-w-xl flex flex-col">
+	<h1 class="text-4xl font-bold mb-8">
+		Conseils d'accesibilité aléatoire
+	</h1>
+
+	<button type="button" on:click={() => console.log('tiep')} class="mx-auto px-4 py-2 bg-black text-white rounded-md hover:bg-gray-800 transition cursor-pointer">
+		Générer
+	</button>
+</section>
